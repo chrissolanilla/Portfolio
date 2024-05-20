dab3eba3be465d07781cce8cb0906efcafd1e2c9
Christopher Solanilla <chrissolanilla@gmail.com>
2024-05-20 14:37:34 -0400

    fixes mobile view and adds semantic tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,133 +53,152 @@
 </script>
 
 
-    <div id="vanta-element" class="intro">
-    <!-- Your content here -->
-    </div>
+<div id="vanta-element" class="intro">
+</div>
 
-    <div class="intro">
-        <div class="name-container">
-            <h2 class="hi">Hi, I'm</h2>
-            <h1 class="name">Christopher Solanilla,</h1>
-            <h2 class="who">a developer</h2>
-            <div class="links">
-                <a href="https://github.com/chrissolanilla">GitHub</a>
-                <a href="https://bustopher.itch.io/" target="_blank" >Itch.io</a>
-                <a href="https://www.linkedin.com/in/christopher-solanilla-837299224/">LinkedIn</a>
-                <a href="mailto:chrissolanilla@gmail.com">Email</a>
-            </div>
-        </div>
-        <button id="down" on:click={scrollToContent}>
-            <svg width="192" height="96" version="1.1" viewBox="-2 -2 196 100" xmlns="http://www.w3.org/2000/svg">
-                <path d="m12 12 84 72 84-72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" style="paint-order:stroke fill markers"></path>
-            </svg>
-        </button>
+<header class="intro">
+    <div class="name-container">
+        <h2 class="hi">Hi, I'm</h2>
+        <h1 class="name">Christopher Solanilla,</h1>
+        <h2 class="who">a developer</h2>
+        <nav class="links">
+            <a href="https://github.com/chrissolanilla">GitHub</a>
+            <a href="https://bustopher.itch.io/" target="_blank" >Itch.io</a>
+            <a href="https://www.linkedin.com/in/christopher-solanilla-837299224/">LinkedIn</a>
+            <a href="mailto:chrissolanilla@gmail.com">Email</a>
+        </nav>
     </div>
-    <div class="content">
+    <button id="down" on:click={scrollToContent}>
+        <svg width="192" height="96" version="1.1" viewBox="-2 -2 196 100" xmlns="http://www.w3.org/2000/svg">
+            <path d="m12 12 84 72 84-72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" style="paint-order:stroke fill markers"></path>
+        </svg>
+    </button>
+</header>
+<main class="content">
     <h1 class="projects">Projects</h1>
-    <div class="card-container">
+        <div class="card-container">
+            <section class="card">
+                <a href="https://bustopher.itch.io/yatagarasu-sunshine" target="_blank" class="">
+                    <div class="thumbnail3" >
+                        <img src="screnshot1.png" alt="screenshot">
+                    </div>
+                    <div class="title">Yatagarasu Sunshine</div>
+                    <p class="desc">A third person 3D bullet-hell game that was made in two days during the FIEA Game Jam. The game was made in Unreal Engine 5 and runs on windows and Linux x86. </p>
+                </a>
 
-        <a href="https://bustopher.itch.io/yatagarasu-sunshine" target="_blank" class="card">
-            <div class="thumbnail3" style="--url: url('/thumbnails/tanks.png');">
-                <img src="screnshot1.png" alt="screenshot">
-            </div>
-            <div class="title">Yatagarasu Sunshine</div>
-            <p class="desc">A third person 3D bullet-hell game that was made in two days during the FIEA Game Jam. The game was made in Unreal Engine 5 and runs on windows and Linux x86. </p>
-        </a>
+            </section>
 
-        <a href="https://codeclash.app" target="_blank" class="card">
-            <div class="thumbnail2" style="--url: url('/thumbnails/tanks.png');">
-                <img src="codeclashgame.PNG" alt="screenshot">
-            </div>
-            <div class="title">Code Clash</div>
-            <p class="desc">An online competative programing game where users can battle each other in a 1v1 to see who can solve leetcode problems faster. Elo system and ranking included.</p>
-        </a>
-        <a href="https://chessconnect.xyz/threeD/test.html" target="_blank" class="card">
-            <div class="thumbnail" style="--url: url('/thumbnails/chess10.png');">
-                <img src="chessconnect3d.PNG" alt="screenshot">
-            </div>
-            <div class="title">Chess Connect</div>
-            <p class="desc">A Chess theme'd contact manager that also includes a physics engine written in vanilla javascript at this page.</p>
-        </a>
-        
-        <a href="https://chibichain.com" target="_blank" class="card">
-            <div class="thumbnail2" style="--url: url('/thumbnails/game-of-life.png');">
-                <img src="Chibichain.PNG" alt="screenshot">
-            </div>
-            <div class="title">Chibi Chain</div>
-            <p class="desc">A free linktree alternative with an anime theme and extra features such as adding songs to your page.</p>
-        </a>
-        
-        <a href="https://kawaiikulture.shop" target="_blank" class="card">
-            <div class="thumbnail" style="--url: url('/thumbnails/softbody-tetris.png');">
-                <img src="kawaiikulture.PNG" alt="screenshot">
-            </div>
-            <div class="title">Kawaii Kulture</div>
-            <p class="desc">My own personal drop shipping business where I sell anime clothes and merchandise from japan.</p>
-        </a>
+            <section class="card">
+                <a href="https://codeclash.app" target="_blank" class="">
+                    <div class="thumbnail2" >
+                        <img src="codeclashgame.PNG" alt="screenshot">
+                    </div>
+                    <div class="title">Code Clash</div>
+                    <p class="desc">An online competative programing game where users can battle each other in a 1v1 to see who can solve leetcode problems faster. Elo system and ranking included.</p>
+                </a>
+
+            </section>
+
+            <section class="card">
+                <a href="https://chessconnect.xyz/threeD/test.html" target="_blank" class="">
+                    <div class="thumbnail">
+                        <img src="chessconnect3d.PNG" alt="screenshot">
+                    </div>
+                    <div class="title">Chess Connect</div>
+                    <p class="desc">A Chess theme'd contact manager that also includes a physics engine written in vanilla javascript at this page.</p>
+                </a>
+
+            </section>
+            
+            <section class="card">
+                <a href="https://chibichain.com" target="_blank" class="">
+                    <div class="thumbnail2" >
+                        <img src="Chibichain.PNG" alt="screenshot">
+                    </div>
+                    <div class="title">Chibi Chain</div>
+                    <p class="desc">A free linktree alternative with an anime theme and extra features such as adding songs to your page.</p>
+                </a>
+
+            </section>
+            
+            <section class="card">
+                <a href="https://kawaiikulture.shop" target="_blank" class="">
+                    <div class="thumbnail">
+                        <img src="kawaiikulture.PNG" alt="screenshot">
+                    </div>
+                    <div class="title">Kawaii Kulture</div>
+                    <p class="desc">My own personal drop shipping business where I sell anime clothes and merchandise from japan.</p>
+                </a>
+
+            </section>
     </div>
+</main>
+<!--button to scroll to other parts of the website-->
+<div class = "OtherSection">
+    <button id="down" on:click={scrollToContent}>
+        <svg width="192" height="96" version="1.1" viewBox="-2 -2 196 100" xmlns="http://www.w3.org/2000/svg">
+            <path d="m12 12 84 72 84-72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" style="paint-order:stroke fill markers"></path>
+        </svg>
+    </button>
+    <h1>While you're here, try checking out some cool features of this site</h1>
+    <div class="content2">
+
+        <button class= "btn btn-primary" on:click={goToChat}>Real Time Chat Room</button>
+        <button class= "btn btn-primary" on:click={goToClock}>Clock Tower</button>
     </div>
-    <!--button to scroll to other parts of the website-->
-    <div class = "OtherSection">
-        <button id="down" on:click={scrollToContent}>
-            <svg width="192" height="96" version="1.1" viewBox="-2 -2 196 100" xmlns="http://www.w3.org/2000/svg">
-                <path d="m12 12 84 72 84-72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" style="paint-order:stroke fill markers"></path>
-            </svg>
-        </button>
-        <h1>While you're here, try checking out some cool features of this site</h1>
-        <div class="content2">
-    
-            <button class= "btn btn-primary" on:click={goToChat}>Real Time Chat Room</button>
-            <button class= "btn btn-primary" on:click={goToClock}>Clock Tower</button>
-        </div>
-    </div>
+</div>
 
-    <style>
-        .btn-primary {
-  --btn-color: #ff7cc4; /* Primary button color set to #ff7cc4 */
-}
+<style>
 
-.btn {
-  display: inline-flex;
-  height: 3rem;
-  min-height: 3rem;
-  flex-shrink: 0;
-  cursor: pointer;
-  user-select: none;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0.5rem; /* Adjusted for simplicity */
-  border-color: transparent;
-  padding: 0 1rem;
-  text-align: center;
-  font-size: 0.875rem;
-  line-height: 1em;
-  gap: 0.5rem;
-  font-weight: 600;
-  text-decoration-line: none;
-  transition-duration: 200ms;
-  transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
-  border-width: 1px; /* Adjusted for simplicity */
-  transition-property: color, background-color, border-color, opacity, box-shadow, transform;
-  box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
-  background-color: var(--btn-color); /* Use the primary color for the background */
-  color: #fff; /* Assuming white text color for contrast */
-}
+    :global(body) {
+        overflow-x: hidden;
+    }
 
-button, [role="button"] {
-  cursor: pointer;
-}
+    .btn-primary {
+    --btn-color: #ff7cc4; /* Primary button color set to #ff7cc4 */
+    }
 
-button, [type="button"], [type="reset"], [type="submit"] {
-  -webkit-appearance: button;
-  background-color: transparent;
-  background-image: none;
-}
+    .btn {
+        display: inline-flex;
+        height: 3rem;
+        min-height: 3rem;
+        flex-shrink: 0;
+        cursor: pointer;
+        user-select: none;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        border-radius: 0.5rem; /* Adjusted for simplicity */
+        border-color: transparent;
+        padding: 0 1rem;
+        text-align: center;
+        font-size: 0.875rem;
+        line-height: 1em;
+        gap: 0.5rem;
+        font-weight: 600;
+        text-decoration-line: none;
+        transition-duration: 200ms;
+        transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+        border-width: 1px; /* Adjusted for simplicity */
+        transition-property: color, background-color, border-color, opacity, box-shadow, transform;
+        box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+        background-color: var(--btn-color); /* Use the primary color for the background */
+        color: #fff; /* Assuming white text color for contrast */
+    }
 
-button, select {
-  text-transform: none;
-}
+    button, [role="button"] {
+    cursor: pointer;
+    }
+
+    button, [type="button"], [type="reset"], [type="submit"] {
+    -webkit-appearance: button;
+    background-color: transparent;
+    background-image: none;
+    }
+
+    button, select {
+        text-transform: none;
+    }
 
     .OtherSection {
         display: flex;
@@ -210,7 +229,7 @@ button, select {
         opacity: 1; /* Adjust the opacity to make it visible */
         transition: opacity 0.3s ease; /* Transition for the opacity change */
     }
-        .name-container > .name {
+    .name-container > .name {
         font-size: calc(var(--scale) * 2);
     }
     .hi {
@@ -237,10 +256,10 @@ button, select {
     }
 
     .card-container {
-    display: flex !important;
-    flex-wrap: wrap !important;
-    gap: 16px !important;
-    justify-content: center !important;
+        display: flex !important;
+        flex-wrap: wrap !important;
+        gap: 16px !important;
+        justify-content: center !important;
     }
 
     .card {
@@ -258,29 +277,28 @@ button, select {
         text-align: initial !important;
         background: var(--content-bg) !important;
         text-decoration: none !important;
-        width: calc((100% - 32px) / 3) !important;
+        
         margin-bottom: 32px !important;
     }
-
+    
     .card:hover {
         transform: translateY(-5px);
         box-shadow: 0 4px 8px rgba(0,0,0,0.2);
     }
-
+    
     .thumbnail img {
         width: 100%;
         height: auto;
         border-radius: 4px;
     }
-
+    
     .thumbnail2 img {
         width: 100%;
         height: auto;
         border-radius: 4px;
     }
     .thumbnail3 {
-        width: 100%;
-        height: 75%;
+        height: 61%;
         border-radius: 4px;
         padding-bottom: 0%;
         margin-bottom: 0%;
@@ -292,7 +310,7 @@ button, select {
         padding-bottom: 0%;
         margin-bottom: 0%;
     }
-
+    
     .links a {
         padding: 12px;
     }
@@ -301,25 +319,35 @@ button, select {
         font-weight: bold;
         margin-top: 8px;
     }
-
+    
     .desc {
         margin-top: 4px;
         color: var(--secondary-fg);
     }
-
+    
     @media (max-width: 768px) {
-        .card {
-            width: calc(100% - 16px); /* Full width on smaller screens */
+        .card-container {
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        section {
+            width: 22rem;
         }
     }
-
+    @media (min-width: 801px) {
+        .card {
+            max-width: calc((100% - 32px) / 3);
+        }        
+    }
+    
     #vanta-element {
-    position: fixed; /* or 'absolute' if that works better for your layout */
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: -1000000; /* Behind everything else */
+        position: fixed; /* or 'absolute' if that works better for your layout */
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1000000; /* Behind everything else */
     }
 
 
